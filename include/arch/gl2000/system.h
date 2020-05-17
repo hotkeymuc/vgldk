2a51e8537d61d802ffcf8ffbaca610946aa3527c
Bernhard Slawik <hotkey@spr.at>
2020-05-17 21:14:35 +0200

    Added several systems

---
+++ include/arch/gl2000/system.h
@@ -0,0 +1,26 @@
+#ifndef __GL2000_H
+#define __GL2000_H
+/*
+Header for
+	VTech PreComputer 2000
+	YENO Mister X2
+	VTech Genius LEADER 2000
+	VTech Genius LEADER 2000 Compact
+	VTech Genius LEADER 2000 PLUS
+	(CompuSavant?)
+
+for use with SDCC compiler
+
+2020-01-21 by Bernhard "HotKey" Slawik
+*/
+
+//#define ARCH GL2000
+
+#define DISPLAY_COLS 20
+#define DISPLAY_ROWS 2
+
+volatile __at (0xdce0) unsigned char KEY_STATUS;	// Controls reading from the keyboard on 2000 (put 0xc0 into it, wait for it to become 0xd0)
+volatile __at (0xdce4) unsigned char KEY_CURRENT;	// Holds the current key code on 2000
+
+
+#endif
---
+++ include/arch/gl6000sl/system.h
@@ -0,0 +1,8 @@
+/*
+
+Stub
+
+Memory
+E000 - EBB7	LCD video memory
+
+*/
