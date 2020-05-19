cfe79341de550fddce4a15f17ca68a52019e6a50
Bernhard Slawik <hotkey@spr.at>
2020-05-19 21:58:59 +0200

    clear

--- include/arch/gl2000/lcd.h
+++ include/arch/gl2000/lcd.h
@@ -23,6 +23,8 @@ TODO:
 #define byte unsigned char
 #define word unsigned short
 
+#define clear lcd_clear
+
 // HDD44780 definitions
 #define LCD_CLEARDISPLAY 0x01
 #define LCD_RETURNHOME 0x02
@@ -428,5 +430,6 @@ void putchar(byte c) {
 	}
 	
 }
+
 
 #endif //__LCD_H
