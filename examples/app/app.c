6c5025333df066987a38c96637f844ab1f937b29
Bernhard Slawik <hotkey@spr.at>
2020-06-07 17:20:20 +0200

    App

---
+++ examples/app/app.c
@@ -0,0 +1,15 @@
+/*
+	A simple "Hello World" for the VGLDK
+	
+	2020-01-22 Bernhard "HotKey" Slawik
+*/
+
+#include <vgldk.h>
+#include <stdiomin.h>
+
+void main() __naked {
+	
+	printf("Hello World!");
+	
+	while(1) { }
+}
