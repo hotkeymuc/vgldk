ce8acac273d0e1d2c1fd5c3f4075baf108c9cce8
Bernhard Slawik <hotkey@spr.at>
2020-08-07 00:12:54 +0200

    Proof-of-concept app

--- examples/app/app_hello.c
+++ examples/app/app_hello.c
@@ -4,15 +4,53 @@
 	2020-01-22 Bernhard "HotKey" Slawik
 */
 
+// Forward declarations
+//int main(int argc, char *argv[]);	// as defined in monitor.c
+//void main() __naked {	
+//int main();
+//void main();
+
+/*
+void app_start() __naked {
+	__asm
+		
+		jp _main;
+	__endasm;
+}
+*/
+
+int app_start(int argc, char *argv[]) {	// as defined in monitor.c
+	
+	(void)argc;
+	(void)argv;
+	
+	
+	__asm
+		call _vgldk_init
+		//call _main;
+	__endasm;
+	
+	
+	return 0x42;
+}
+
+
+
 #include <vgldk.h>
 #include <stdiomin.h>
 
 //void main() __naked {
-int main() {
+void main() {
+	char c;
 	
 	printf("Hello World!\n");
+	c = getchar();
+	
+	
+	printf("Key to end");
+	c = getchar();
 	
 	//while(1) { }
 	//return;
-	return 4;
+	//return c;
 }
