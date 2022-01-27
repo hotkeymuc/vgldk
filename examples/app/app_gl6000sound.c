ef8e15e0bc8c67a48a91e1bffa46411cf4eb5347
Bernhard Slawik <hotkey@spr.at>
2022-01-27 20:04:35 +0100

    I can hear something easily now. But HOW does the data go through the ports...

--- examples/app/app_gl6000sound.c
+++ examples/app/app_gl6000sound.c
@@ -6,6 +6,18 @@
 	Be careful: The Speech chip has an internal amplifier. When hitting the wrong resonance, you can almost short the power supply!
 	Believe me! The LCD flashed weirdly, the speaker made an "Oooof!"-sound and the USB power supply browned out!
 	
+	R 0x10 == 0xF9
+	R 0x11 == 0xFF
+	R 0x60 == 0x0B
+	W 0x10 := 0x00
+	
+	R 0x10, R 0x11, R 0x60, ...
+	
+	W 0x10 := 0x04
+	R 0x60 == 0x0F
+	
+	
+	
 	2022-01-19 Bernhard "HotKey" Slawik
 */
 
@@ -42,14 +54,31 @@ int app_start(void *v_stdout_putchar, void *v_stdin_getchar) {
 #include <ports.h>
 #include <hex.h>
 
+
 byte check_port(byte p) {
 	//char c;
-	byte v = port_in(p);
+	int i;
+	byte v = 0x00;
+	byte v_old = 0x00;
 	
-	printf("0x"); printf_x2(p); printf(" = 0x"); printf_x2(v); printf("\n");
-	//c = getchar();
+	for(i = 0; i < 1000; i++) {
+		v = port_in(p);
+		if ((i == 0) || (v != v_old)) {
+			if (i == 0) {
+				printf("0x"); printf_x2(p); printf(" = ");
+			} else {
+				putchar(',');
+			}
+			printf_x2(v);	//printf("\n");
+			//c = getchar();
+			v_old = v;
+		}
+	}
+	printf("\n");
 	return v;
 }
+
+/*
 
 void monitor_ports() {
 	byte v_10;
@@ -76,6 +105,58 @@ void monitor_ports() {
 	}
 	
 }
+*/
+
+int tsp_frame;
+
+void tsp_put(byte v) {
+	byte c;
+	
+	//printf_x2(v);
+	//c = getchar();
+	
+	port_out(0x10, 0x00);
+	
+	// After OUT 0x10, 0x00:
+	//check_port(0x60);	// 0x0F
+	//check_port(0x10);	// 0xF9
+	
+	//port_out(0x10, 0x04);
+	//check_port(0x10);	// After OUT 0x10, 0x04: 0xFF, 0xFD, 0xFF, 0xFD, ...
+	
+	//port_out(0x11, v);	// Actually output data to the pins
+	
+	//check_port(0x60);	// 0x0F
+	//check_port(0x10);	// 0xF9
+	
+	
+	//port_out(0x11, (v * 0x55) % 0x100);	// Testing
+	
+	port_out(0x10, 0x04);	// Works: Put 0x00, then 0x04, then enter data!
+	
+	//port_out(0x60, 0 + (tsp_frame++) % 6);	// Testing
+	
+	
+	// After OUT 0x10, 0x00:
+	//check_port(0x10);	// 0xF9
+	
+	// After OUT 0x10, 0x04:
+	//check_port(0x10);	// 0xFC (sound stopped?) -OR- 0xFD ... 0xFF ... 0xFD (sound running?)
+	//check_port(0x11);	// 0xFF
+	//check_port(0x21);	// 0xFF
+	//check_port(0x60);	// 0x0F
+	//check_port(0x62);	// 0xFF
+	
+	while(port_in(0x10) == 0xfd) { }	// Wait for wiggle-state :)
+	
+	port_out(0x11, v);	// Actually output data to the pins
+	
+	//check_port(0x11);	// 0xFC if sound stopped -OR- wiggling 0xFD ... 0xFF ... 0xFD
+	//check_port(0x10);	// 0xFC if sound stopped -OR- wiggling 0xFD ... 0xFF ... 0xFD
+	
+	//port_out(0x10, 0x04);
+	
+}
 
 //void main() __naked {
 //void main() {
@@ -92,23 +173,42 @@ int main(int argc, char *argv[]) {
 	printf("Hello!\n");
 	//c = getchar();
 	
+	// Boot:
 	port_out(0x22, 0x00);
 	port_out(0x21, 0xe0);
+	
 	port_out(0x23, 0x60);
-	//LCD
 	port_out(0x23, 0x60);
 	port_out(0x21, 0xe0);
+	//LCD
 	port_out(0x61, 0xd0);
 	port_out(0x10, 0x00);
 	
-	//check_port(0x21);
-	
+	check_port(0x21);	// 0xff
 	port_out(0x21, 0xff);
+	
 	port_out(0x29, 0x01);
 	port_out(0x43, 0x02);
 	port_out(0x23, 0x20);
 	
-	//for(i = 0; i < 4; i++) { check_port(0x21); }
+	//for (i=0; i < 3; i++) {
+		check_port(0x21);	// 0xff
+		check_port(0x60);	// 0x0b
+	//}
+	
+	port_out(0x62, 0x00);
+	port_out(0x10, 0x00);
+	check_port(0x10);	// 0xf9
+	port_out(0x10, 0x04);
+	
+	//for (i=0; i < 3; i++) {
+		check_port(0x21);	// 0xff
+		check_port(0x60);	// 0x0F by now
+	//}
+	
+	
+	// End of init
+	
 	
 	/*
 	// This actually produces glitchy sounds (and eventually browns out LOUDLY)
@@ -133,97 +233,58 @@ int main(int argc, char *argv[]) {
 	*/
 	
 	
-	port_out(0x62, 0x00);
+	/*
+	for(i = 0; i < 10; i++) {
+		printf("\nLoop: ");
+		tsp_put(0x0e);
+		tsp_put(0x02);
+		tsp_put(0x21);
+		tsp_put(0x67);
+		tsp_put(0x4B);
+		
+		tsp_put(0x11);
+		tsp_put(0x1B);
+		tsp_put(0x00);
+		tsp_put(0xfd);
+		tsp_put(0x21);
+		tsp_put(0x7d);	// Burp
+		tsp_put(0x21);
+	}
+	*/
 	
+	/*
+	// Just speculating...
+	port_out(0x51, 0x08);
+	o = (byte *)0x4000 + 0x00c0;	// + 0x0040;	//4320 - 12*4;	// Around 0x4320 it gets crazy!
+	*/
 	
-	port_out(0x51, 8);
-	o = (byte *)0x4000 + 0x0040;	//4320 - 12*4;	// Around 0x4320 it gets crazy!
+	//@TODO: Real frames?
+	// spss011d.pdf, 6.1, page 180
+	//	Parameter	Energy	Repeat	Pitch 	K1	K2	K3	K4	K5	K6	K7	K8	K9	K10	K11	K12
+	//	# Bits		4		1		7		6	6	5	5	4	4	4	3	3	3	0	0
+	// 56 bits = 7 bytes?
 	
-	// 400B:	"Blubb"
-	// 400A:	"tschwschwschw....."
-	// 402E:	"tschwschwschw....."
-	// 4032:	"tschwschwschw....."
-	// 4038:	"Quuuoouuu!" + Brown-out
+	// Speech data can be found in ROM:0x6D141+ (out 0x51,0x1b, mem[0x5141...])
+	port_out(0x51, 0x1B);	// OUT 0x51, 0x1B	-> maps ROM:0x6C000 to CPU:0x4000
+	o = (byte *)0x5141;		// MEM:0x5141 now shows ROM:0x6D141 = BOING-sound
+	//o = (byte *)0x5000;		// MEM:0x5000 now shows ROM:0x6D000 = sounds and stuff
 	
 	while(true) {
+		v = *o;
 		
-		
-		/*
-		//check_port(0x60);
-		//do {
-		for(i = 0; i < 10; i++) {
-			c = port_in(0x60);	// 0x0B / 0x0F
-			//if (c != 0x0b) printf_x2(c);
-		}
-		//} while (c & 0x04);	// == 0x00);
-		*/
-		
-		
-		
-		// INT
-		//check_port(0x10);
-		/*
-		for(i = 0; i < 10; i++) {
-			c = port_in(0x10);	// 0x0B / 0xF9 / 0xFD
-		}
-		*/
-		
-		v = *o;	o++;
-		
-		
-		//port_out(0x22, 0x00);
-		//port_out(0x21, 0xe0);
-		//port_out(0x23, 0x60);
-		
-		//printf_x2(c);//putchar(' ');
-		//c = getchar();
-		
-		port_out(0x10, 0x00);
-		
-		port_out(0x11, v);
-		
-		port_out(0x10, 0xff);
-		
-		
-		// Wait for 0x10 to turn from 0xFF to 0xFC / 0xFD?
-		
-		
-		//@TODO: Real frames?
-		// spss011d.pdf, 6.1, page 180
-		//	Parameter	Energy	Repeat	Pitch 	K1	K2	K3	K4	K5	K6	K7	K8	K9	K10	K11	K12
-		//	# Bits		4		1		7		6	6	5	5	4	4	4	3	3	3	0	0
-		// 56 bits = 7 bytes?
-		
-		//if ((word)o >= 0x438a) {
-		//if ((word)o >= 0x4280) {
-		//if ((word)o >= 0x422c) {
+		tsp_put(v);
 		
 		//if ((word)o % 2 == 0) {
 			printf_x4((word)o); putchar(' '); printf_x2(v);
 			c = getchar();
 			printf("\n");
+		
+			//check_port(0x10);
+		
 		//}
 		//monitor_ports();
 		
-		
-		//port_out(0x62, 0x00);
-		
-		
-		//port_out(0x61, 0xd0);
-		//port_out(0x62, 0xff);
-		//port_out(0x10, 0x00);
-		
-		/*
-		putchar(' ');
-		
-		c = port_in(0x10);
-		printf_x2(c);
-		
-		printf("\n");
-		//putchar(' ');
-		
-		//c = keyboard_inkey();
-		*/
+		o++;
 	}
 	
 	
