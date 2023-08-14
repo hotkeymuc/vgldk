58df68c6710a579351fbba22cb05ac8a2af591bf
Bernhard Slawik <hotkey@spr.at>
2023-08-14 16:47:45 +0200

    ...

---
+++ examples/monitor/monitor_experiments.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+# Get monitor communication
+sys.path.append('/z/data/_code/_c/V-Tech/vgldk.git/tools')
+from send2monitor import *
+
+
+def put(t):
+	print(t)
+
+if __name__ == '__main__':
+	
