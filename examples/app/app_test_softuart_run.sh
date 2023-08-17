0882deba536dce18920c4dffc3bd767aef8828ff
Bernhard Slawik <hotkey@spr.at>
2023-08-18 00:05:09 +0200

    back to softuart finetuning using app and monitor...

---
+++ examples/app/app_test_softuart_run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#make app
+# "upload" also creates the app
+make upload
+
+# Go to receiver right away
+#picocom --baud 9600 --imap 8bithex,spchex /dev/ttyUSB0
+#picocom --baud 19200 --imap 8bithex,spchex,nrmhex /dev/ttyUSB0
+picocom --baud 19200 --imap spchex,tabhex,crhex,lfhex,8bithex,nrmhex /dev/ttyUSB0
