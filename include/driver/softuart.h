6b80639807733074e11469fba3de688aac4f1e35
Bernhard Slawik <hotkey@spr.at>
2023-08-18 00:00:53 +0200

    Fine-tuned 19200 on GL4000

--- include/driver/softuart.h
+++ include/driver/softuart.h
@@ -8,6 +8,7 @@
 	Tested and working on GL6000SL with 9600 and 19200 baud
 	
 	TODO:
+	* Use "OUTI" for fast port output? (Outputs (HL) to port C, decrements B, increments HL)
 	OK	Re-create TX 9600 baud: L=23, 9569 baud
 	OK	Determine max. TX baud: L=1, 21739 baud
 	OK	Try TX at 19200 baud
@@ -21,15 +22,19 @@
 */
 
 
+// Please define these values outside:
+//#define SOFTUART_SERIES 4000
+//#define SOFTUART_SERIES 6000
 //#define SOFTUART_BAUD 9600
 //#define SOFTUART_BAUD 19200
+
 
 // Timeouts (for "semi-blocking" reads)
 #define SOFTUART_TIMEOUT_START 250	// Timeout while waiting for start bit
 #define SOFTUART_TIMEOUT_STOP 100	// Timeout while waiting for stop bit
 
 // For debugging: Send a "mark" pulse for gathering timings using an oscilloscope
-//#define SOFTUART_MARK softuart_set_low(); softuart_set_high();
+//#define SOFTUART_MARK softuart_set_HIGH(); softuart_set_LOW();
 #define SOFTUART_MARK ;
 
 // For the special case of compiling an app (VGLDK_SERIES=0) you can manually specify the hardware series for softuart
@@ -48,20 +53,57 @@
 	#define SOFTUART_BITS_MASK 0xff	// Bitmask on parallel port
 	
 	#define SOFTUART_STROBE_MASK 0x04	// Bitmask (OR) on control port to strobe LOW (inverse for HIGH)
-	#define SOFTUART_BITS_LOW 0x00	// Bits (on printer port) to set for serial "HIGH"
-	#define SOFTUART_BITS_HIGH 0xff	// Bits (on printer port) to set for serial "LOW"
+	#define SOFTUART_BITS_HIGH 0x00	// Bits (on printer port) to set for serial data = 1 / LOW
+	#define SOFTUART_BITS_LOW 0xff	// Bits (on printer port) to set for serial data = 0 / HIGH
 	
 	// Timing configuration
 	#if SOFTUART_BAUD == 9600
 		// 9600 baud:
-		#define SOFTUART_DELAY_RX 16	// GL4000 at 9600 baud: 16-17
+		#define SOFTUART_RX_DELAY 16	// GL4000 at 9600 baud: 16-17
+		#define SOFTUART_RX_DELAY_EDGE 1	// Usually 1
+		
 		#define SOFTUART_DELAY_TX 22	// GL4000 at 9600 baud: L=23	=> 836us/8 =>  9569 baud
-		#define SOFTUART_TX_DELAY_POST nop
+		#define SOFTUART_TX_OPS nop
+	
 	#elif SOFTUART_BAUD == 19200
 		// 19200 baud:
-		#define SOFTUART_DELAY_RX 6	// GL4000 at 19200 baud: 5-6
-		#define SOFTUART_DELAY_TX 3	// GL4000 at 19200 baud: L=3	=> 408us/8 => 19607 baud
-		#define SOFTUART_TX_DELAY_POST nop
+		#warning "SOFTUART_BAUD 19200 has timing problems. Only working for GL6000 as of now. Use 9600 baud."
+		
+		#define SOFTUART_RX_DELAY 6	// GL4000 at 19200 baud: 5-6
+		#define SOFTUART_RX_DELAY_EDGE 0	// Usually 1, but in this case: 0, to not miss the highest bit
+		
+		
+		#define SOFTUART_DELAY_TX 1	// GL4000 at 19200 baud: 1-3?
+		// Not reconstructable: L=3	=> 408us/8 => 19607 baud: Need some (4-6?) DELAY_POST nops
+		// L=3 with 5 nops: 58,8us => 17006 baud
+		// L=2 with 4 nops: 56,0us => 17857 baud
+		// L=3 with 2 nops: 56,8us => 17605 baud
+		// L=2 with 2 nops: 53,0us => 18867 baud
+		// L=2 with 1 nops: 52-53us => 18866-19230 baud
+		// L=1 with 3 nops: 55,25us => 18433 baud
+		// L=1 with 0 nops: 53,25us => 18779 baud
+		//#define SOFTUART_TX_OPS nop
+		
+		//#define SOFTUART_TX_OPS_PRE_START nop
+		#define SOFTUART_TX_OPS_POST_START nop\
+			nop\
+			nop\
+			nop\
+			nop\
+			nop\
+			nop\
+			nop
+		
+		#define SOFTUART_TX_OPS_PRE_STOP nop\
+			nop\
+			nop\
+			nop\
+			nop\
+			nop\
+			nop\
+			nop
+		
+		//#define SOFTUART_TX_OPS_POST_STOP nop
 	#else
 		#error "SOFTUART_BAUD not defined/supported on GL4000!"
 	#endif
@@ -72,8 +114,8 @@
 	__sfr __at SOFTUART_PORT_CONTROL softuart_port_control;
 	
 	// Macros are faster than calling a function, because there is no stack management necessary
-	#define softuart_set_high() softuart_set(SOFTUART_BITS_HIGH)
-	#define softuart_set_low() softuart_set(SOFTUART_BITS_LOW)
+	#define softuart_set_LOW() softuart_set(SOFTUART_BITS_LOW)
+	#define softuart_set_HIGH() softuart_set(SOFTUART_BITS_HIGH)
 	#define softuart_set(d) {\
 		softuart_port_data = d;\
 		softuart_port_latch = SOFTUART_BITS_MASK;\
@@ -101,7 +143,8 @@
 	}
 	*/
 	
-	#define softuart_get_high() ((softuart_port_latch & 0x20) > 0)
+	#define softuart_get_LOW() ((softuart_port_latch & 0x20) > 0)
+	#define softuart_get_HIGH() ((softuart_port_latch & 0x20) == 0)
 	/*
 	byte softuart_get() {
 		if ((softuart_port_latch & 0x20) == 0) return 1;	// HIGH
@@ -119,19 +162,23 @@
 	#define SOFTUART_PORT_CONTROL 0x23
 	
 	#define SOFTUART_BITS_MASK 0xff	// Bitmask on parallel port
-	#define SOFTUART_BITS_LOW 0xff	// Bits (on printer port) to set for serial "HIGH"
-	#define SOFTUART_BITS_HIGH 0x00	// Bits (on printer port) to set for serial "LOW"
+	#define SOFTUART_BITS_HIGH 0xff	// Bits (on printer port) to set for serial "HIGH"
+	#define SOFTUART_BITS_LOW 0x00	// Bits (on printer port) to set for serial "LOW"
 	
 	#if SOFTUART_BAUD == 9600
 		// 9600 baud:
-		#define SOFTUART_DELAY_RX 17	// GL6000SL at 9600 baud: 17 works
+		#define SOFTUART_RX_DELAY 17	// GL6000SL at 9600 baud: 17 works
+		#define SOFTUART_RX_DELAY_EDGE 1	// Usually 1
+		
 		#define SOFTUART_DELAY_TX 21	// GL6000SL at 9600 baud: 20-22 works
-		#define SOFTUART_TX_DELAY_POST nop
+		#define SOFTUART_TX_OPS nop
 	#elif SOFTUART_BAUD == 19200
 		// 19200 baud:
-		#define SOFTUART_DELAY_RX 7	// GL6000SL at 19200 baud: 7 works
+		#define SOFTUART_RX_DELAY 7	// GL6000SL at 19200 baud: 7 works
+		#define SOFTUART_RX_DELAY_EDGE 1	// Usually 1
+		
 		#define SOFTUART_DELAY_TX 0	// GL6000SL at 19200 baud: 0 with 0-3 post-nops works;  1 with no post-nops ALMOST works, but is too slow on last MSB
-		#define SOFTUART_TX_DELAY_POST 
+		#define SOFTUART_TX_OPS 
 	#else
 		#error "SOFTUART_BAUD not defined/supported on GL6000SL!"
 	#endif
@@ -143,8 +190,8 @@
 	__sfr __at SOFTUART_PORT_CONTROL softuart_port_control;
 	
 	// Macros are faster than calling a function, because there is no stack management necessary
-	#define softuart_set_high() {\
-		softuart_set(SOFTUART_BITS_HIGH)\
+	#define softuart_set_LOW() {\
+		softuart_set(SOFTUART_BITS_LOW)\
 		__asm\
 			; Usually writing 0x00 requires 3 extra NOPS, but empirically thats not needed any more?\
 			;nop\
@@ -152,8 +199,8 @@
 			;nop\
 		__endasm;\
 	}
-	#define softuart_set_low() {\
-		softuart_set(SOFTUART_BITS_LOW)\
+	#define softuart_set_HIGH() {\
+		softuart_set(SOFTUART_BITS_HIGH)\
 		__asm\
 		__endasm;\
 	}
@@ -168,7 +215,7 @@
 	}
 	
 	/*
-	void softuart_set_high() {
+	void softuart_set_LOW() {
 		softuart_port_control = 0x20;
 		softuart_port_latch = 0xff;
 		softuart_port_data = 0xff;
@@ -214,7 +261,7 @@
 	}
 	
 	
-	void softuart_set_low() {
+	void softuart_set_HIGH() {
 		
 		__asm
 			ld	a, #0x20				; Disable latch?
@@ -256,7 +303,7 @@
 	}
 	*/
 	
-	#define softuart_get_high() ((softuart_port_status & 0x80) == 0)
+	#define softuart_get_LOW() ((softuart_port_status & 0x80) == 0)
 	/*
 	void softserial_get_high() {
 		__asm
@@ -290,16 +337,18 @@ void softuart_tx_delay() {
 		;	ld	l, #22	; => 816us/8 =>  9803 baud
 		;	ld	l, #23	; => 836us/8 =>  9569 baud (works as 9600 baud!)
 		;	ld	l, #25	; => 880us/8 =>  9090 baud
-	#if #SOFTUART_DELAY_TX > 0
+		#if SOFTUART_DELAY_TX > 0
 		ld	l, #SOFTUART_DELAY_TX
 		
 		_softuart_tx_delay_loop:
 			dec	l
 			jr	nz, _softuart_tx_delay_loop
-	#endif
+		#endif
 		
+		#ifdef SOFTUART_TX_OPS
 		; Allow some custom NOP slides
-		SOFTUART_TX_DELAY_POST
+		SOFTUART_TX_OPS
+		#endif
 		
 		pop hl
 	__endasm;
@@ -314,7 +363,7 @@ void softuart_rx_delay() {
 	// GL4000 at 9600 baud with MARK at each bit: 9
 	// GL4000 at 19200 baud: 5
 	// GL4000 at ~38400 baud: 1
-	for(i = 0; i < SOFTUART_DELAY_RX; i++) {
+	for(i = 0; i < SOFTUART_RX_DELAY; i++) {
 		__asm
 			nop
 		__endasm;
@@ -332,10 +381,10 @@ void softuart_rx_delay() {
 	*/
 }
 void softuart_rx_delayEdge() {
-	// Delay between edge and middle of bit while receiving
+	// Delay between edge and middle of bit while receiving (usually a single nop)
 	
 	byte i;
-	for(i = 0; i < 1; i++) {
+	for(i = 0; i < SOFTUART_RX_DELAY_EDGE; i++) {
 		__asm
 			nop
 		__endasm;
@@ -349,25 +398,49 @@ void softuart_sendByte(byte d) {
 	
 	byte b;
 	
-	// Start bit (HIGH)
-	softuart_set_high();
+	#ifdef SOFTUART_TX_OPS_PRE_START
+	__asm
+		SOFTUART_TX_OPS_PRE_START
+	__endasm;
+	#endif
+	
+	// Start bit (LOW)
+	softuart_set_LOW();
 	softuart_tx_delay();
+	
+	#ifdef SOFTUART_TX_OPS_POST_START
+	__asm
+		SOFTUART_TX_OPS_POST_START
+	__endasm;
+	#endif
 	
 	// Data bits
 	for(b = 0; b < 8; b++) {
 		if (d & 0x01) {
-			softuart_set_low();
+			softuart_set_HIGH();
 		} else {
-			softuart_set_high();
+			softuart_set_LOW();
 		}
 		
 		d >>= 1;
 		softuart_tx_delay();
 	}
 	
-	// Stop bit (LOW)
-	softuart_set_low();
+	#ifdef SOFTUART_TX_OPS_PRE_STOP
+	__asm
+		SOFTUART_TX_OPS_PRE_STOP
+	__endasm;
+	#endif
+	
+	// Stop bit (HIGH)
+	softuart_set_HIGH();
 	softuart_tx_delay();
+	
+	#ifdef SOFTUART_TX_OPS_POST_STOP
+	__asm
+		SOFTUART_TX_OPS_POST_STOP
+	__endasm;
+	#endif
 }
 
 
@@ -377,9 +450,9 @@ int softuart_receiveByte() {
 	byte i;
 	byte b;
 	
-	// Wait for start bit (HIGH/0) to happen
+	// Wait for start bit (LOW/0) to happen
 	i = 0;
-	while(!softuart_get_high()) {
+	while(softuart_get_HIGH()) {
 		// Still LOW - Handle timeout
 		i++;
 		if (i > SOFTUART_TIMEOUT_START) return -1;
@@ -404,11 +477,12 @@ int softuart_receiveByte() {
 		// Mark bit
 		//SOFTUART_MARK;
 		
-		if (!softuart_get_high()) {
-			// LOW/"1"
+		if (softuart_get_HIGH()) {
+			// HIGH/"1"
 			b |= 0x80;
 		} else {
-			// HIGH/"0"
+			// LOW/"0"
+			
 			// Caution: Keep this part of the "if" clause roughly the same number of CPU cycles!
 			//b &= 0xfe;	// Not required as b is 0 from the beginning
 			__asm
@@ -427,10 +501,10 @@ int softuart_receiveByte() {
 	// Mark the end of data bits
 	SOFTUART_MARK;
 	
-	// Wait for stop bit (LOW/1) to actually happen
+	// Wait for stop bit (HIGH/1) to actually happen
 	i = 0;
-	while(softuart_get_high()) {
-		// Still HIGH - Handle timeout
+	while(softuart_get_LOW()) {
+		// Still not HIGH - Handle timeout
 		i++;
 		if (i > SOFTUART_TIMEOUT_STOP) return -2;
 	}
