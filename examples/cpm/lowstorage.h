0be4965d710ae77129ab1317821893e783341cd1
Bernhard Slawik <hotkey@spr.at>
2023-08-23 14:24:54 +0200

    Dynamic lowstorage-restoration

--- examples/cpm/lowstorage.h
+++ examples/cpm/lowstorage.h
@@ -1,7 +1,7 @@
 // Stub! Please re-run cpm_make.py to generate it properly!
 const word lowstorage_origin = 0x0;
 const word lowstorage_size = 0x005c;
-const byte lowstorage_data[%d] = {
+const byte lowstorage_data[0x5c] = {
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
