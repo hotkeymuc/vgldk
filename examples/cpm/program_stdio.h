cb1b670343319467f8fd8038b4ca64b0b4b4e049
Bernhard Slawik <hotkey@spr.at>
2023-09-04 00:44:04 +0200

    using sdcc stdio

--- examples/cpm/program_stdio.h
+++ examples/cpm/program_stdio.h
@@ -13,24 +13,9 @@ It contains wrappers for BDOS function calls.
 
 #include "program.h"
 
-void putchar(char b);
-char getchar();
-
-// Helpers
-void puts(const char *s);
-//char *gets(char *pc);
-//void gets(char *pc, byte max_size);
-void gets(char *pc);
-
-void printf(const char *s);
-//void printf_d(char *pc, byte d);
-//void printf_d(byte d);
-
-
-
 
 // BDOS STDIO
-void putchar(char b) __naked {
+void _putchar(char b) __naked {
 	(void)b;	// Silence the compiler
 	__asm
 		
@@ -57,7 +42,7 @@ void putchar(char b) __naked {
 	//return;
 }
 
-char getchar() __naked {
+char _getchar() __naked {
 	__asm
 		;push af
 		;push hl
@@ -80,15 +65,7 @@ char getchar() __naked {
 	//return getchar_tmp;
 }
 
-void puts(const char *s) {
-	//@TODO: Use BDOS_FUNC_C_WRITESTR 9 = Output string (terminated by '$')
-	while (*s != 0) {
-		putchar(*s++);
-	}
-	putchar('\n');	//@TODO: Use correct End-of-Line (\n, \r or both)
-}
-
-
+/*
 volatile char *gets_tmp;
 //char *gets(char *pc) {
 //void gets(char *pc, byte max_size) {
@@ -132,96 +109,20 @@ void gets(char *pc) {
 	// Terminate string with zero
 	*(pc+l) = 0;
 	
-	/*
-	// Manually implement gets using BDOS getchar()
-	//@TODO: Handle cursor position/insert/overwrite/del/backspace
-	
-	char c;
-	//myscroll_counter = LCD_ROWS - lcd_y - 1;	// Reset scroll counter, i.e. user has enough time to read current page
-	while(1) {
-		
-		
-		//@FIXME: something goes wrong here with the stack...
-		//#error "continue bugfixing here! Something corrupts the stack after typing a key"
-		putchar('[');	// for debugging!
-		c = getchar();
-		putchar(']');	// for debugging!
-		
-		// Force local echo? getchar() outputs to conout anyway!
-		#ifdef PROGRAM_GETS_LOCAL_ECHO
-		putchar(c);
-		#endif
-		if ((c < 0x20) || (c > 'z')) {
-			program_printf_x2(c);
-		}
-		
-		if ( (c == 8) || (c == 127) ) {
-			// Backspace/DEL
-			if (pc > pcs) {
-				pc--;
-				
-				//	if (lcd_x > 0) {
-				//		lcd_x--;
-				//		vgl_lcd_set_cursor();
-				//	}
-				
-			}
-			continue;
-		}
-		
-		if ((c == '\n') || (c == '\r') || (c == 0)) {
-			// End of string
-			
-			//@FIXME: Add '\r' or '\n' at end of string in CP/M?
-			
-			// Terminate string
-			*pc = 0;
-			
-			//return pcs;
-			return;
-		}
-		
-		// Add char
-		*pc++ = c;
-	}
-	return pcs;
-	*/
 	//return pc;	// Return original pointer
 }
-
-
-//#include <hex.h>
-// puts wihtout new line
-void printf(const char *s) {
-	//@TODO: Use BDOS_FUNC_C_WRITESTR 9 = Output string (terminated by '$')
-	while (*s != 0) {
-		putchar(*s++);
-	}
-}
-
-/*
-byte program_hexDigit(byte c) {
-	if (c < 10) return ('0'+c);
-	return 'A' + (c-10);
-}
-void program_printf_x2(byte b) {
-	putchar(program_hexDigit(b >> 4));
-	putchar(program_hexDigit(b & 0x0f));
-}
 */
 
-/*
-//void program_printf_d(char *pc, byte d) {
-void program_printf_d(byte d) {
-	byte i;
-	i = 100;	// Maximum decimal digit (1/10/100/1000/...)
-	while(i > 0) {
-		putchar('0' + ((d / i) % 10));
-		i /= 10;
-	}
-}
-*/
 
+// Make compatible to stdio
+int getchar(void) {
+	return _getchar();
+}
+int putchar(int c) {
+	_putchar(c);
+	return 1;
+}
+#include <stdio.h>
 
 
 #endif	// __PROGRAM_STDIO_H
